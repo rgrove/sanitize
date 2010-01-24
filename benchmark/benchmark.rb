7b13294a3b38d6a3db29b8dcd6d2096720e4bffb
Ryan Grove <ryan@wonko.com>
2010-01-24 00:35:48 -0800

    Add a simple benchmark suite, mostly borrowed from Loofah (http://github.com/flavorjones/loofah).

---
+++ benchmark/benchmark.rb
@@ -0,0 +1,75 @@
+#!/usr/bin/env ruby
+
+# To run benchmark.rb, you'll need the "hitimes" and "loofah" gems.
+#
+# The benchmarks and much of the code here are patterned after the benchmarks in
+# the Loofah project (http://github.com/flavorjones/loofah), which has the
+# following license:
+#
+# Copyright (c) 2009 Mike Dalessio, Bryan Helmkamp
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+DIR          = File.expand_path(File.dirname(__FILE__))
+HTML_BIG     = File.read("#{DIR}/html/test_big.html")
+HTML_SMALL   = File.read("#{DIR}/html/test_small.html")
+HTML_SNIPPET = 'this is a tiny <em>snippet</em> of html'
+
+require "#{DIR}/helpers"
+
+class TestLoofah < Measure
+  include TestSet
+
+  def bench(html, times, is_fragment)
+    clear
+
+    if is_fragment
+      measure('Loofah :strip', times) do
+        Loofah.scrub_fragment(html, :strip).to_s
+      end
+    else
+      measure('Loofah :strip', times) do
+        Loofah.scrub_document(html, :strip).to_s
+      end
+    end
+
+    measure('Sanitize.clean! (no pre-init)', times) do
+      Sanitize.clean!(html, Sanitize::Config::RELAXED)
+    end
+
+    s = Sanitize.new(Sanitize::Config::RELAXED)
+
+    measure('Sanitize.clean! (pre-init)', times) do
+      s.clean!(html)
+    end
+  end
+end
+
+puts "Loofah version  : #{Loofah::VERSION}"
+puts "Sanitize version: #{Sanitize::VERSION}"
+puts "Nokogiri version: #{Nokogiri::VERSION_INFO.inspect}"
+puts
+
+benchmarks = [TestLoofah.new]
+
+puts "-- Rehearsal --"
+benchmarks.each {|bm| bm.test_set(:scale => 10) }
+
+puts "-- Benchmark --"
+benchmarks.each {|bm| bm.test_set }
---
+++ benchmark/helpers.rb
@@ -0,0 +1,84 @@
+# Most of the code here (with slight tweaks) is from the Loofah project
+# (http://github.com/flavorjones/loofah), and is included under the following
+# license:
+#
+# Copyright (c) 2009 Mike Dalessio, Bryan Helmkamp
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+$:.unshift("#{DIR}/../lib")
+
+require 'rubygems'
+require 'hitimes'
+require 'loofah'
+require 'sanitize'
+
+class Measure
+  def initialize
+    clear
+  end
+
+  def clear
+    @first_run = true
+    @baseline  = nil
+  end
+
+  def measure(name, ntimes)
+    if @first_run
+      printf "  %-30s %7s  %8s  %5s\n", "", "total", "single", "rel"
+      @first_run = false
+    end
+
+    timer = Hitimes::TimedMetric.new(name)
+
+    timer.start
+    ntimes.times {|j| yield }
+    timer.stop
+
+    if @baseline
+      printf "  %30s %7.3f (%8.6f) %5.2fx\n", timer.name, timer.sum, timer.sum / ntimes, timer.sum / @baseline
+    else
+      @baseline = timer.sum
+      printf "  %30s %7.3f (%8.6f) %5s\n", timer.name, timer.sum, timer.sum / ntimes, "-"
+    end
+
+    timer.sum
+  end
+end
+
+module TestSet
+  def test_set(options = {})
+    scale = options[:scale] || 1
+
+    n = 100 / scale
+    puts "  Big HTML doc (#{HTML_BIG.length} bytes) x #{n}"
+    bench(HTML_BIG, n, false)
+    puts
+
+    n = 1000 / scale
+    puts "  Small HTML fragment (#{HTML_SMALL.length} bytes) x #{n}"
+    bench HTML_SMALL, n, true
+    puts
+
+    n = 10_000 / scale
+    puts "  Tiny HTML fragment (#{HTML_SNIPPET.length} bytes) x #{n}"
+    bench(HTML_SNIPPET, n, true)
+    puts
+  end
+end
