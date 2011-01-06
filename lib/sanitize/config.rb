674996aa7da3a7daec6d553b88c39603ea3cc475
Ryan Grove <ryan@wonko.com>
2011-01-05 22:17:19 -0800

    Holy crap it's 2011.

--- lib/sanitize/config.rb
+++ lib/sanitize/config.rb
@@ -1,5 +1,5 @@
 #--
-# Copyright (c) 2010 Ryan Grove <ryan@wonko.com>
+# Copyright (c) 2011 Ryan Grove <ryan@wonko.com>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the 'Software'), to deal
