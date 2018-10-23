eeb059e3af009ded6c4dbba548eb692c54176695
srghma <srghma@gmail.com>
2018-10-23 12:54:42 +0300

    feat(#190): add video tag to relaxed config -> add

--- lib/sanitize/config/relaxed.rb
+++ lib/sanitize/config/relaxed.rb
@@ -7,7 +7,7 @@ class Sanitize
         address article aside bdi bdo body caption col colgroup data del div
         figcaption figure footer h1 h2 h3 h4 h5 h6 head header hgroup hr html
         img ins main nav rp rt ruby section span style summary sup table tbody
-        td tfoot th thead title tr wbr
+        td tfoot th thead title tr wbr video source
       ],
 
       :allow_doctype => true,
@@ -27,7 +27,9 @@ class Sanitize
         'table'    => %w[align bgcolor border cellpadding cellspacing frame rules sortable summary width],
         'td'       => %w[abbr align axis colspan headers rowspan valign width],
         'th'       => %w[abbr align axis colspan headers rowspan scope sorted valign width],
-        'ul'       => %w[type]
+        'ul'       => %w[type],
+        'video'    => %w[controls autoplay width height],
+        'source'   => %w[src type]
       ),
 
       :protocols => merge(BASIC[:protocols],
