dc1ec0cde2a1401cb93a49a2c3d537b7c8c3da58
Ajmal Sali <ajmalmsali@users.noreply.github.com>
2020-11-17 18:43:42 +0530

    sup is already part of BASIC
    Let's avoid a duplicate,

--- lib/sanitize/config/relaxed.rb
+++ lib/sanitize/config/relaxed.rb
@@ -6,7 +6,7 @@ class Sanitize
       :elements => BASIC[:elements] + %w[
         address article aside bdi bdo body caption col colgroup data del div
         figcaption figure footer h1 h2 h3 h4 h5 h6 head header hgroup hr html
-        img ins main nav rp rt ruby section span style summary sup table tbody
+        img ins main nav rp rt ruby section span style summary table tbody
         td tfoot th thead title tr wbr
       ],
 
