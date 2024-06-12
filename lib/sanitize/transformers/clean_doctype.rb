26ae4db32da42086354cc596f17dfec079f3a42c
Mike Dalessio <mike.dalessio@gmail.com>
2024-06-12 18:16:06 -0400

    fix: set the DOCTYPE by removing the DTD and creating a new one
    because GNOME/libxml2@8d0aaf4 prevents us from changing the node name
    in libxml >= 2.13.0.
    
    See discussion in #3230 for context.

--- lib/sanitize/transformers/clean_doctype.rb
+++ lib/sanitize/transformers/clean_doctype.rb
@@ -9,7 +9,11 @@ class Sanitize; module Transformers
 
     if node.type == Nokogiri::XML::Node::DTD_NODE
       if env[:config][:allow_doctype]
-        node.name = 'html'
+        if node.name != "html"
+          document = node.document
+          node.unlink
+          document.create_internal_subset("html", nil, nil)
+        end
       else
         node.unlink
       end
